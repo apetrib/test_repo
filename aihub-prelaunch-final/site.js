db72e00fcbbe0ce590ed56c677f34221fc7abe43
apetrib <apetri.bogdan@gmail.com>
2026-08-22 12:20:26 +0300

    Republish Practical AI copy refinements

--- aihub-prelaunch-final/site.js
+++ aihub-prelaunch-final/site.js
@@ -9,7 +9,7 @@ if(menu&&links){
 }
 
 (()=>{
-  const version='visual-6';
+  const version='visual-7';
   if(document.documentElement.dataset.aihubVisual===version)return;
   document.documentElement.dataset.aihubVisual=version;
   ['01.css','02.css','03.css','04.css','05.css','06.css'].forEach(file=>{
